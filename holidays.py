bf8717686d15c2af701f0919c05423d2e4701e6f
Benyomin Ginzburg <benyomin.94@gmail.com>
2017-05-15 11:37:35 +0300

    Time.sleep

--- holidays.py
+++ holidays.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import requests
+import time
 from datetime import datetime, timedelta
 import pytz
 import re
@@ -899,9 +900,13 @@ def tu_bav(loc, lang):
 
 
 def rosh_hashanah(loc, lang):
+    time.sleep(5)
     rosh_hashanah_name = get_holiday_name(index(14, loc), lang)
+    time.sleep(5)
     rosh_date = get_holiday_data(index(14, loc), loc, lang)
+    time.sleep(5)
     rosh_time = rosh_ash_shavout(index(14, loc), loc, lang)
+    time.sleep(5)
     rosh_hashanah_str = f'{rosh_hashanah_name}\n' \
                         f'{rosh_date}\n' \
                         f'{rosh_time}'
